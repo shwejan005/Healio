c4b95b1a9b270062514fa85da94709e8091c29bb
Shwejan Kumar Enjamuri <shwejannn@gmail.com>
2025-02-21 22:44:00 +0530

    Added scroll animations to all pages and fixed some bugs

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import type { Metadata } from "next";
 import { ThemeProvider } from "next-themes";
 import { League_Spartan } from "next/font/google";
 import "./globals.css";
+import ReactLenis from "lenis/react";
 
 const leagueSpartan = League_Spartan({
   subsets: ["latin"],
@@ -21,20 +22,22 @@ export default function RootLayout({
   return (
     <ConvexClerkProvider>
       <html lang="en" suppressHydrationWarning>
-        <body className={`${leagueSpartan.className} antialiased bg-background`}>
-          <ThemeProvider
-            attribute="class"
-            defaultTheme="system"
-            enableSystem
-            disableTransitionOnChange
-          >
-              <div className="min-h-screen">
-                <main className="min-h-screen">{children}</main>
-              </div>
-              
+        <ReactLenis root>
+          <body className={`${leagueSpartan.className} antialiased bg-background`}>
+            <ThemeProvider
+              attribute="class"
+              defaultTheme="system"
+              enableSystem
+              disableTransitionOnChange
+            >
+                <div className="min-h-screen">
+                  <main className="min-h-screen">{children}</main>
+                </div>
+                
 
-          </ThemeProvider>
-        </body>
+            </ThemeProvider>
+          </body>
+        </ReactLenis>
       </html>
     </ConvexClerkProvider>
   );
